4542f589ad3ccbc0faa48c5602684a0d80ddeb44
tushargoyal-hack1 <56203660+tushargoyal-hack1@users.noreply.github.com>
2019-10-06 00:43:39 +0530

    binding of ip and port

--- sender.py
+++ sender.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2
-
+import os
 import socket 
 sender_ip="192.168.10.177"
 myport=9999
@@ -8,8 +8,9 @@ myport=9999
 # for tcp dgram hatake stream in caps
 # below ,ethod wth argument creating a soket  called s 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+# making object of socket for ip and port bind
 # now conenctng ip and port 
-
+print("Ip and port will be binded")
 s.bind((sender_ip,myport))
+
 
